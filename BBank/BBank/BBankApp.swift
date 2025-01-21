a843f5d38490acccce16dbb29da884c79dff7cbf
Egor <alyanov.egor2018@yandex.ru>
2025-01-21 23:11:11 +0300

    proj init

---
+++ BBank/BBank/BBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBankApp.swift
+//  BBank
+//
+//  Created by Alexander on 21.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
